df121d659471110c8957283814db0d850ee580f3
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-20 23:05:36 +0530

    initial code

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        // Creating nodes manually
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+
+        list.head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+
+        // Display the linked list
+        System.out.println("Linked List:");
+        list.display();
+    }
+}
